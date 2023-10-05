5e9924536d6a153e2b1c7e8999fb6a6b61ede450
David Nene <64870164+davidnene@users.noreply.github.com>
2023-10-05 20:40:04 +0300

    Configure views

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import ToDoHeader from './components/ToDoHeader.vue';
 
 </script>
 
 <template>
+  <ToDoHeader/>
   <RouterView />
 </template>
 
